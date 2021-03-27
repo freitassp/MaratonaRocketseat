7affec4f6cd2e9a0e60e9a0d10e634b47571a891
freitassp <priettosamuel@gmail.com>
2021-03-27 20:57:13 -0300

    criando package.json

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require("express")
-const { request } = require("node:http")
+//const { request } = require("node:http")
 const server = express()
+
 
 //request, response
 server.get('/', (request, response) => {
-    return response.send('Oiiiiiiiie')
+    return response.send('OIIII!!!!')
 })
 
 server.listen(3000, () => console.log('rodando'))
