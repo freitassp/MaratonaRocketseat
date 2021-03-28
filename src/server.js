f3e5acf90a5ce2510441e117b7a45ababa08ca59
freitassp <priettosamuel@gmail.com>
2021-03-28 16:21:32 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require("express")
+
 //const { request } = require("node:http")
 const server = express()
 
 
 //request, response
 server.get('/', (request, response) => {
-    return response.send('OIIII!!!!')
+    //console.log(__dirname + "/views/index.html")
+    return response.sendFile(__dirname + "/views/index.html")
 })
 
 server.listen(3000, () => console.log('rodando'))
